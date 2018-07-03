7927eb74860a2b50c522a5d270979b7fb1541a88
Jaydaar <jaydaar@gmail.com>
2018-07-03 16:24:19 -0400

    Fixed the look of the dropdown menu on both the mobile and full screen.

--- js/init.js
+++ js/init.js
@@ -1,8 +1,9 @@
-(function($){
-  $(function(){
+(function($) {
+    $(function() {
 
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
+        $('.sidenav').sidenav();
+        $('.parallax').parallax();
+        $('.dropdown-trigger').dropdown({ hover: true });
 
-  }); // end of document ready
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
