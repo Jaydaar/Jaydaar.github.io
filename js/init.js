933921f8fa5093f50538a31d93f9e77598251065
Jaydaar <jaydaar@gmail.com>
2018-07-05 19:01:20 -0400

    Added sololearn certificates.

--- js/init.js
+++ js/init.js
@@ -5,6 +5,7 @@
         $('.parallax').parallax();
         $('.dropdown-trigger').dropdown({ hover: true });
         $('.modal').modal();
+        $('.tabs').tabs();
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space
