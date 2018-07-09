5e0568f5ca3f6d70b89b22166049750f5b6027b7
Jaydaar <jaydaar@gmail.com>
2018-07-09 12:56:40 -0400

    Edited the videos page.

--- js/init.js
+++ js/init.js
@@ -1,11 +1,11 @@
 (function($) {
     $(function() {
-
         $('.sidenav').sidenav();
         $('.parallax').parallax();
         $('.dropdown-trigger').dropdown({ hover: true });
         $('.modal').modal();
+        M.AutoInit(document.body)
         $('.tabs').tabs();
-
+        $('.carousel').carousel({ 'dist': 0, 'indicators': true });
     }); // end of document ready
 })(jQuery); // end of jQuery name space
